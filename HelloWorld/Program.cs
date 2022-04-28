712818de225a26b98efc1f6e7c7aaf4e6993de94
Mihai Maier <maiermihai90@gmail.com>
2022-04-28 20:00:28 +0300

    Change

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!3");
         }
     }
 }
